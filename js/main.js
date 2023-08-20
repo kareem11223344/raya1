91af597bd519149bf3bcd9194412e11d9c96f5c9
Karem Ahmed <113023102+kareem11223344@users.noreply.github.com>
2023-08-20 20:52:36 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var typed = new Typed("#auto-type", {
+    strings: ["تصميم مواقع", "تصميم ابلكيشن للموبايل","برمجة التطبيقات","برمجة تطبيقات الاندرويد"," ios برمجة تطبيقات "],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+  });
